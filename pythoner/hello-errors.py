fc8bc26437194df0e7f9e5e5b18520af1c13a3ba
luankefei <luankefei@laiye.com>
2015-11-09 19:49:48 +0800

    test

--- pythoner/hello-errors.py
+++ pythoner/hello-errors.py
@@ -16,7 +16,7 @@ class IndexHandler(tornado.web.RequestHandler):
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers = [(r'/', IndexHandler)])
+    app = tornado.web.Application(handlers=[(r'/', IndexHandler)])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
